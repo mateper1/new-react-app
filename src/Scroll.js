cbd70390c774298cd536d24f0843a82b51425694
mateper <66566571+mateper@users.noreply.github.com>
2020-07-07 19:33:47 +0200

    border

--- src/Scroll.js
+++ src/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return (
-		<div style={{overflowY:'scroll', height:'600px'}}>
+		<div style={{overflowY:'scroll', height:'650px'}}>
 			{props.children}
 		</div>
 		);
