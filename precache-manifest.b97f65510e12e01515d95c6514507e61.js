18ccb05d10f8a2f0ff0c986499294b0e5f71dff7
mateper <66566571+mateper@users.noreply.github.com>
2020-07-07 17:53:57 +0200

    Updates

---
+++ precache-manifest.b97f65510e12e01515d95c6514507e61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5846e22507db80f9433dfc33dc828d",
+    "url": "/new-react-app/index.html"
+  },
+  {
+    "revision": "a3e14d51adf70ac7716f",
+    "url": "/new-react-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e26ba708f3cc0abc8d43",
+    "url": "/new-react-app/static/css/main.62612ab4.chunk.css"
+  },
+  {
+    "revision": "a3e14d51adf70ac7716f",
+    "url": "/new-react-app/static/js/2.d4eeb839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-react-app/static/js/2.d4eeb839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e26ba708f3cc0abc8d43",
+    "url": "/new-react-app/static/js/main.ab30ceb0.chunk.js"
+  },
+  {
+    "revision": "f4fdc0d6fe4dd2a679e9",
+    "url": "/new-react-app/static/js/runtime-main.9686fb9a.js"
+  },
+  {
+    "revision": "06edb304ef25a28a1d57dfd6bace55ca",
+    "url": "/new-react-app/static/media/Potra-E0Ye.06edb304.woff"
+  }
+]);
